50b207a4d1b7bf87cc2d4c222797649ac9be776c
nimgnas <sangminyn@gmail.com>
2023-08-03 21:44:29 +0900

    [level 0] Title: 저주의 숫자 3, -직접코드추가

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -8,3 +8,17 @@ function solution(n) {
 
   return answer;
 }
+
+//코드추가
+function solution(n) {
+  var answer = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0) {
+      n++;
+    }
+    if (String(i).includes("3") & (i % 3 != 0)) {
+      n++;
+    }
+  }
+  return n;
+}
