c6be7efa398bc688a0217c17259654c7ea700914
Chaejung Kim <whenucan35@gmail.com>
2023-08-12 00:45:42 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,9 +1,4 @@
 function solution(cards1, cards2, goal) {
-    var answer = 'Yes';
-    goal.forEach((ele) => {
-        if (cards1[0] == ele)  cards1.shift()
-        else if (cards2[0] == ele) cards2.shift()
-        else answer = 'No'
-    })
+    var answer = '';
     return answer;
 }
