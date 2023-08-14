5c9b8a63bb90cb0e727118db2238a2f11e2ab9e1
blcklamb <whenucan35@gmail.com>
2023-08-14 14:12:53 +0900

    [lv1] Title: 카드 뭉치, -코드직접추가

--- KimChaeJung/프로그래머스/lv1/135808. 과일 장수/159994. 카드 뭉치/카드 뭉치.js
+++ KimChaeJung/프로그래머스/lv1/135808. 과일 장수/159994. 카드 뭉치/카드 뭉치.js
@@ -1,4 +1,9 @@
 function solution(cards1, cards2, goal) {
-    var answer = '';
+    var answer = 'Yes';
+    goal.forEach((ele) => {
+        if (cards1[0] == ele)  cards1.shift()
+        else if (cards2[0] == ele) cards2.shift()
+        else answer = 'No'
+    })
     return answer;
 }
--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++
@@ -1,9 +0,0 @@
-function solution(cards1, cards2, goal) {
-    var answer = 'Yes';
-    goal.forEach((ele) => {
-        if (cards1[0] == ele)  cards1.shift()
-        else if (cards2[0] == ele) cards2.shift()
-        else answer = 'No'
-    })
-    return answer;
-}
