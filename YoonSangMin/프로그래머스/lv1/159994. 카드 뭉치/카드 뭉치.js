3d76866d7a07054ce9c209aeb2c519cc76905328
nimgnas <sangminyn@gmail.com>
2023-08-14 15:36:54 +0900

    [8월3주1주차] 잘못된 코드 위치 변경

--- YoonSangMin/프로그래머스/lv1/159994. 카드 뭉치/카드 뭉치.js
+++ YoonSangMin/프로그래머스/lv1/159994. 카드 뭉치/카드 뭉치.js
@@ -8,21 +8,3 @@ function solution(cards1, cards2, goal) {
   }
   return answer;
 }
-
-// 코드추가
-function solution(n, m, section) {
-  let count = 0;
-  const arr = Array.from(Array(n + 1).fill(null));
-
-  section.forEach((el) => {
-    arr[el] = 1;
-  });
-
-  section.forEach((el) => {
-    if (arr[el]) {
-      arr.fill(null, el, el + m);
-      count++;
-    }
-  });
-  return count;
-}
