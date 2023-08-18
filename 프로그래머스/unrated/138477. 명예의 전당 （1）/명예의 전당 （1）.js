e93ac078a42d8f60dc6793208711fb90543df6cc
blcklamb <whenucan35@gmail.com>
2023-08-18 23:19:12 +0900

    [lv1] Title: 명예의 전당 (1), -직접코드추가, 결과추가

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,24 +1,23 @@
 function solution(k, score) {
-    var answer = [];
-    const arr = []
-    
-    for(let i = 0; i < k; i++) {
-        if(i >= score.length) break
-        
-        arr.push(score[i])
-        arr.sort((a, b) => b - a)
-        answer.push(arr[arr.length - 1])
-    }
+  var answer = [];
+  const arr = [];
 
-    
-    for(let i = k; i < score.length; i++) {
-        if(arr[arr.length - 1] < score[i]) {
-            arr.pop()
-            arr.push(score[i])
-            arr.sort((a, b) => b - a)
-        }
-            answer.push(arr[arr.length - 1])
+  for (let i = 0; i < k; i++) {
+    if (i >= score.length) break;
+
+    arr.push(score[i]);
+    arr.sort((a, b) => b - a);
+    answer.push(arr[arr.length - 1]);
+  }
+
+  for (let i = k; i < score.length; i++) {
+    if (arr[arr.length - 1] < score[i]) {
+      arr.pop();
+      arr.push(score[i]);
+      arr.sort((a, b) => b - a);
     }
-    
-    return answer;
+    answer.push(arr[arr.length - 1]);
+  }
+
+  return answer;
 }
