0b62abc11133bf9098785bdaaa488e92542fc8f7
Chaejung Kim <whenucan35@gmail.com>
2023-08-20 18:33:35 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,4 +1,21 @@
 function solution(ingredient) {
-    var answer = 0;
+    let answer = 0;
+    let left_stack = [];
+    let right_stack = ingredient.slice(0).reverse();
+    while (right_stack.length > 0) {
+        const rlidx = right_stack.length - 1
+        if (right_stack[rlidx] === 3 && right_stack[rlidx -1] === 1){
+            const last_idx = left_stack.length - 1
+            if (left_stack[last_idx] === 2 && left_stack[last_idx-1] === 1){
+                left_stack.pop()
+                left_stack.pop()
+                right_stack.pop()
+                right_stack.pop()
+                answer += 1
+                continue
+            }
+        }
+        left_stack.push(right_stack.pop());
+    }
     return answer;
 }
