b9503a113cb43ec01f1a41b3f363d486178d5bb4
blcklamb <whenucan35@gmail.com>
2023-08-22 22:05:27 +0900

    [lv1] Title: 명예의 전당 (1), -직접코드추가, 결과추가

--- KimChaeJung/프로그래머스/lv1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ KimChaeJung/프로그래머스/lv1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,23 +1,22 @@
 function solution(k, score) {
-  var answer = [];
-  const arr = [];
-
-  for (let i = 0; i < k; i++) {
-    if (i >= score.length) break;
-
-    arr.push(score[i]);
-    arr.sort((a, b) => b - a);
-    answer.push(arr[arr.length - 1]);
-  }
-
-  for (let i = k; i < score.length; i++) {
-    if (arr[arr.length - 1] < score[i]) {
-      arr.pop();
-      arr.push(score[i]);
-      arr.sort((a, b) => b - a);
-    }
-    answer.push(arr[arr.length - 1]);
-  }
-
-  return answer;
+  var top_k = [];
+  return score.reduce((a, b) => {
+    top_k.push(b);
+    top_k.sort((a, b) => a - b);
+    if (top_k.length > k) top_k.shift();
+    a.push(top_k[0]);
+    return a;
+  }, []);
 }
+
+// 결과
+```
+'평균 시간: 1.131ms'
+'평균 메모리: 34.496MB'
+'최고 시간: 3.91ms'
+'최저 시간: 0.07ms'
+'최고 메모리: 36.7MB'
+'최저 메모리: 33.5MB'
+'시간 표준 편차: 7.475'
+'메모리 표준 편차: 13.486'
+```;
