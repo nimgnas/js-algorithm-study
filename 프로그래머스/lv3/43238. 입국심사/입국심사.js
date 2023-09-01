671017421bd0d8d7bb4cebd7f667f5489372cbfd
Chaejung Kim <whenucan35@gmail.com>
2023-09-01 16:03:07 +0900

    [level 3] Title: 입국심사, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.js
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -1,11 +1,12 @@
 function solution(n, times) {
     let [start, end] = [1, Number.MAX_SAFE_INTEGER]
-    const result = []
+    const result = [] 
     while (start <= end){
         let mid = Math.floor((start + end) / 2)
         const total = times.reduce((a, b) => a + Math.floor(mid/b), 0);
         if (total >= n) {
             end = mid - 1
+            
             result.push(mid)
         } else {
             start = mid + 1
