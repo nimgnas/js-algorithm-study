595c56acb5a79b02802bcaa654b3c1ff9dc7417d
Chaejung Kim <whenucan35@gmail.com>
2023-09-04 18:40:00 +0900

    [level 3] Title: 네트워크, Time: 0.56 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,32 @@
+function solution(n, computers) {
+    var answer = 0;
+    const connected = computers.map(ele => 
+                                    ele.map((info, idx) => info === 1 ? idx : '_')
+                                    .filter(e => !isNaN(e))
+                                   )
+    
+    let visited = Array(n).fill(0)
+    
+    function BFS(start, connect, visitedInfo){
+        let queue = [start]
+        visitedInfo[start] = 1
+        while (queue.length > 0){
+            const curr = queue.pop()
+            for (const next of connect[curr]){
+                if (visitedInfo[next] === 1) continue
+                queue.push(next)
+                visited[next] = 1
+            }
+        }
+        
+        return visitedInfo
+    }
+    
+    for (let i = 0; i < n; i++){
+        if (visited[i] === 1) continue
+        visited = BFS(i, connected, visited)
+        answer += 1
+    }
+    
+    return answer;
+}
