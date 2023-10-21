6b272778bad55ecc2c723dd5ae8d09d2eca636f3
blcklamb <whenucan35@gmail.com>
2023-10-21 11:15:29 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, --직접 코드추가

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++
@@ -1,7 +0,0 @@
-// function solution(my_string, n) {
-//     return my_string.slice(-n, my_string.length);
-// }
-
-function solution(my_string, n) {
-    return my_string.substring(my_string.length -n)
-}
