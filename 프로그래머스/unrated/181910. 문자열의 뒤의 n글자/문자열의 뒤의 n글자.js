6877df57eb5032ce920d9c7c7bc4a647c7b37da8
Chaejung Kim <whenucan35@gmail.com>
2023-10-21 10:38:04 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -0,0 +1,7 @@
+// function solution(my_string, n) {
+//     return my_string.slice(-n, my_string.length);
+// }
+
+function solution(my_string, n) {
+    return my_string.substring(my_string.length -n)
+}
