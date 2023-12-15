c4c6a5d1ca927e6365a3546bf38676aa2b875a5e
blcklamb <whenucan35@gmail.com>
2023-12-15 19:01:56 +0900

    [level 2] Title: 배달, --다른사람코드추가

--- KimChaeJung/프로그래머스/lv2/12978. 배달/배달.js
+++ KimChaeJung/프로그래머스/lv2/12978. 배달/배달.js
@@ -27,3 +27,30 @@ function solution(N, road, K) {
 
   return weighInfo[1].filter((ele) => ele <= K).length;
 }
+
+// 다른 사람 코드 - 다익스트라
+function solution(N, road, K) {
+  const totalDist = new Array(N + 1).fill(Infinity);
+  const adj = Array.from({ length: N + 1 }, () => []);
+
+  road.forEach(([a, b, c]) => {
+    adj[a].push({ to: b, dist: c });
+    adj[b].push({ to: a, dist: c });
+  });
+
+  const queue = [{ to: 1, dist: 0 }];
+  totalDist[1] = 0;
+
+  while (queue.length) {
+    let { to, dist } = queue.pop();
+
+    adj[to].forEach((step) => {
+      if (totalDist[step.to] > totalDist[to] + step.dist) {
+        totalDist[step.to] = totalDist[to] + step.dist;
+        queue.push(step);
+      }
+    });
+  }
+
+  return totalDist.filter((dist) => dist <= K).length;
+}
